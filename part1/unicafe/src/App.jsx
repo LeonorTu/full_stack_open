4b772460f55fc8608f51f984d0e02fba086efbfe
Leonor Tu <Jingyi.Tu@outlook.com>
2024-11-04 14:33:34 +0200

    Feat: finished ex1.6

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+function App() {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const increaseGood = () => {
+    setGood(good + 1);
+  };
+  const increaseNeural = () => {
+    setNeutral(neutral+ 1);
+  };
+  const increaseBad = () => {
+    setBad(bad + 1);
+  };
+  return (
+    <>
+      <div>
+        <h1>give feedback</h1>
+        <button onClick={increaseGood}>good</button>
+        <button onClick={increaseNeural}>neutral</button>
+        <button onClick={increaseBad}>bad</button>
+        <h1>statistics</h1>
+        <p>good {good}</p>
+        <p>neutral{neutral}</p>
+        <p>bad {bad}</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
