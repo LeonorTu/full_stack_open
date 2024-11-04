3e684573113d0ac03ae6896a34baeabf328cc24b
Leonor Tu <Jingyi.Tu@outlook.com>
2024-11-04 17:11:02 +0200

    Feat: finished ex1.14, displayed the anecdote with the largest number of votes

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -14,6 +14,7 @@ const App = () => {
 
   const [selected, setSelected] = useState(0);
   const [votes, setVotes] = useState(Array(anecdotes.length).fill(0));
+  const [mostVoteIndex, setMostVoteIndex] = useState(undefined);
 
   const selectRandom = () => {
     setSelected(Math.floor(Math.random() * anecdotes.length));
@@ -23,14 +24,32 @@ const App = () => {
     let updatedVotes = [...votes];
     updatedVotes[selected]++;
     setVotes(updatedVotes);
+    setMostVoteIndex(updatedVotes.indexOf(Math.max(...updatedVotes)));
+  };
+
+  const displayMostVotes = () => {
+    if (mostVoteIndex !== undefined) {
+      return (
+        <div>
+          <h1>Anedote with most votes</h1>
+          <p>{anecdotes[mostVoteIndex]}</p>
+          <p>has {votes[mostVoteIndex]} votes</p>
+        </div>
+      );
+    }
+    return;
   };
 
   return (
     <div>
-      <p>{anecdotes[selected]}</p>
-      <p>has {votes[selected]} votes</p>
-      <button onClick={() => countVotes({ votes, selected })}>vote</button>
-      <button onClick={selectRandom}>next anecdote</button>
+      <div>
+        <h1>Anedote of the day</h1>
+        <p>{anecdotes[selected]}</p>
+        <p>has {votes[selected]} votes</p>
+        <button onClick={() => countVotes({ votes, selected })}>vote</button>
+        <button onClick={selectRandom}>next anecdote</button>
+      </div>
+      {displayMostVotes()}
     </div>
   );
 };
