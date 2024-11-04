626943c7f7f9e2fb2461f8f513bfafaccd4d3fd8
Leonor Tu <Jingyi.Tu@outlook.com>
2024-11-04 14:54:54 +0200

    Feat: finished ex1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -11,30 +11,34 @@ function App() {
   const increaseGood = () => {
     setGood(good + 1);
   };
-  const increaseNeural = () => {
+  const increaseNeutral = () => {
     setNeutral(neutral + 1);
   };
   const increaseBad = () => {
     setBad(bad + 1);
   };
 
-  const all = good + neutral + bad;
-  const average = (good - bad) / all || 0;
-  const positive = (good / all) * 100 || 0;
+  const Statistics = (good, neutral, bad) => {
+    const all = good + neutral + bad;
+    const average = (good - bad) / all || 0;
+    const positive = (good / all) * 100 || 0;
+    return { all, average, positive };
+  };
+
   return (
     <>
       <div>
         <h1>give feedback</h1>
         <button onClick={increaseGood}>good</button>
-        <button onClick={increaseNeural}>neutral</button>
+        <button onClick={increaseNeutral}>neutral</button>
         <button onClick={increaseBad}>bad</button>
         <h1>statistics</h1>
         <p>good {good}</p>
-        <p>neutral{neutral}</p>
+        <p>neutral {neutral}</p>
         <p>bad {bad}</p>
-        <p>all {all}</p>
-        <p>average {average}</p>
-        <p>positive {positive}%</p>
+        <p>all {Statistics(good, neutral, bad).all}</p>
+        <p>average {Statistics(good, neutral, bad).average}</p>
+        <p>positive {Statistics(good, neutral, bad).positive}%</p>
       </div>
     </>
   );
